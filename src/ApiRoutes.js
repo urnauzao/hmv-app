d325a493ba87f22b06173257e80f7e4d9c64f505
Vis Rodrigues <visrodrigues@MacBook-Air-de-Vis.local>
2022-03-27 14:23:56 -0300

    atualizado routes

--- src/ApiRoutes.js
+++ src/ApiRoutes.js
@@ -12,8 +12,8 @@ const ApiRoutes = {
         'POST_CHAMADO_EMERGENCIA': '/v1/pacientes/chamados_emergencia' // '/paciente/chamado_emergencia'
     },
     'QUESTIONARIO_EMERGENCIA': {
-        'GET_NEW_QUESTIONARIO': '/fiap-hmv/v1/questionarios-emergencia/perfil/',  // '/questionario_emergencia/novo',
-        'POST_NEW_QUESTIONARIO': '/fiap-hmv/v1/questionarios-emergencia/perfil/', // '/questionario_emergencia/novo'
+        'GET_NEW_QUESTIONARIO': '/v1/questionarios-emergencia/perfil/',  // '/questionario_emergencia/novo',
+        'POST_NEW_QUESTIONARIO': '/v1/questionarios-emergencia/perfil/', // '/questionario_emergencia/novo'
     },
     'HABITO_SAUDE': {
         'GET_NEW_HABITO': '/v1/habito-saude',  // '/habito_saude/novo',
@@ -26,14 +26,14 @@ const ApiRoutes = {
         'GET_METRICS': '/v1/metricas/medicos/',                                        // '/medico/metricas',
         'GET_QUEST_EMERG_PACIENTE': '/v1/questionarios-emergencia/medico',             // '/medico/questionario_emergencia',
         'GET_HABITO_SAUDE_PACIENTE': '/v1/habitos-saude/medicos/',                     // '/medico/habito_saude',
-        'GET_HISTORICO_PACIENTE': '/fiap-hmv/v1/atendimentos/historicos/pacientes/',   // '/medico/historico',
+        'GET_HISTORICO_PACIENTE': '/v1/atendimentos/historicos/pacientes/',   // '/medico/historico',
         'POST_RELATORIO_ATENDIMENTO' : '/fiap-hmv/v1/atendimentos/medicos'             // '/medico/relatorio_atendimento'
     },
     'ATENDENTE': {
         'GET_METRICS': '/v1/metricas/atendentes',                                                               // '/metricas',
-        'POST_AGENDAMENTO_NEW': '/fiap-hmv/v1/agendamentos/atendentes/',                                        // '/agendamento/novo',
-        'PUT_AGENDAMENTO_SIT': '/fiap-hmv/v1/agendamentos/atendentes/{id}/status/{status_id}',                  // '/agendamento/situacao',
-        'GET_AGENDAMENTO_DEFINIR_MEDICO': '/fiap-hmv/v1/agendamentos/{id_agendamento}/medicos/{id_medico}', // '/agendamento/definir_medico/',
+        'POST_AGENDAMENTO_NEW': '/v1/agendamentos/atendentes/',                                        // '/agendamento/novo',
+        'PUT_AGENDAMENTO_SIT': '/v1/agendamentos/atendentes/{id}/status/{status_id}',                  // '/agendamento/situacao',
+        'GET_AGENDAMENTO_DEFINIR_MEDICO': '/v1/agendamentos/{id_agendamento}/medicos/{id_medico}', // '/agendamento/definir_medico/',
         'GET_AGENDAMENTO_ESTABELECIMENTOS': '/v1/agendamentos/pacientes/{id}/estabelecimentos/enderecos'        // '/agendamento/estabelecimentos'
     }
 
