8032c2f6db19646fdf7cb9096668f2ded7985b88
Vis Rodrigues <visrodrigues@MacBook-Air-de-Vis.local>
2022-03-27 14:46:47 -0300

    atualizado routes

--- src/ApiRoutes.js
+++ src/ApiRoutes.js
@@ -1,40 +1,41 @@
 const ApiRoutes = {
-    // 'APP_URL': 'http://localhost:8000',
+    
     'APP_URL': 'https://d1vg0iaktfp68d.cloudfront.net',
-    // 'API_URL': 'http://localhost:8000/api',
     'API_URL': 'https://d1vg0iaktfp68d.cloudfront.net/fiap-hmv',
     'LOGIN': {
-        'POST_LOGIN': '/v1/login', // '/sanctum/token',
-        'GET_ME': '/v1/usuarios'   // '/usuario/dados'
+        'POST_LOGIN': '/v1/login', 
+        'GET_ME': '/v1/usuarios',   
+        'POST_REGISTER': '/v1/usuarios' 
     },
     'PACIENTE': {
-        'GET_METRICS': '/v1/metricas/pacientes',                       // '/paciente/metricas',
-        'POST_CHAMADO_EMERGENCIA': '/v1/pacientes/chamados_emergencia' // '/paciente/chamado_emergencia'
+        'GET_METRICS': '/v1/metricas/pacientes',                       
+        'POST_CHAMADO_EMERGENCIA': '/v1/pacientes/chamados_emergencia' 
     },
     'QUESTIONARIO_EMERGENCIA': {
-        'GET_NEW_QUESTIONARIO': '/v1/questionarios-emergencia/perfil/',  // '/questionario_emergencia/novo',
-        'POST_NEW_QUESTIONARIO': '/v1/questionarios-emergencia/perfil/', // '/questionario_emergencia/novo'
+        'GET_NEW_QUESTIONARIO': '/v1/questionarios-emergencia/perfil/',  
+        'POST_NEW_QUESTIONARIO': '/v1/questionarios-emergencia/perfil/'  
     },
     'HABITO_SAUDE': {
-        'GET_NEW_HABITO': '/v1/habito-saude',  // '/habito_saude/novo',
-        'POST_NEW_HABITO': '/v1/habito-saude', // '/habito_saude/novo'
+        'GET_NEW_HABITO': '/v1/habito-saude',  
+        'POST_NEW_HABITO': '/v1/habito-saude'  
     },
     'ENDERECO': {
-        'GET_ALL_MY': '/v1/enderecos' // '/endereco/meus'
+        'GET_ALL_MY': '/v1/enderecos' 
     },
     'MEDICO': {
-        'GET_METRICS': '/v1/metricas/medicos/',                                        // '/medico/metricas',
-        'GET_QUEST_EMERG_PACIENTE': '/v1/questionarios-emergencia/medico',             // '/medico/questionario_emergencia',
-        'GET_HABITO_SAUDE_PACIENTE': '/v1/habitos-saude/medicos/',                     // '/medico/habito_saude',
-        'GET_HISTORICO_PACIENTE': '/v1/atendimentos/historicos/pacientes/',   // '/medico/historico',
-        'POST_RELATORIO_ATENDIMENTO' : '/fiap-hmv/v1/atendimentos/medicos'             // '/medico/relatorio_atendimento'
+        'GET_METRICS': '/v1/metricas/medicos/',                                        
+        'GET_QUEST_EMERG_PACIENTE': '/v1/questionarios-emergencia/medico',             
+        'GET_HABITO_SAUDE_PACIENTE': '/v1/habitos-saude/medicos/',                     
+        'GET_HISTORICO_PACIENTE': '/fiap-hmv/v1/atendimentos/historicos/pacientes/',   
+        'POST_RELATORIO_ATENDIMENTO' : '/fiap-hmv/v1/atendimentos/medicos'             
     },
     'ATENDENTE': {
-        'GET_METRICS': '/v1/metricas/atendentes',                                                               // '/metricas',
-        'POST_AGENDAMENTO_NEW': '/v1/agendamentos/atendentes/',                                        // '/agendamento/novo',
-        'PUT_AGENDAMENTO_SIT': '/v1/agendamentos/atendentes/{id}/status/{status_id}',                  // '/agendamento/situacao',
-        'GET_AGENDAMENTO_DEFINIR_MEDICO': '/v1/agendamentos/{id_agendamento}/medicos/{id_medico}', // '/agendamento/definir_medico/',
-        'GET_AGENDAMENTO_ESTABELECIMENTOS': '/v1/agendamentos/pacientes/{id}/estabelecimentos/enderecos'        // '/agendamento/estabelecimentos'
+        'GET_METRICS': '/v1/metricas/atendentes',                                                               
+        'POST_AGENDAMENTO_NEW': '/fiap-hmv/v1/agendamentos/atendentes/',                                        
+        'PUT_AGENDAMENTO_SIT': '/fiap-hmv/v1/agendamentos/atendentes/{id}/status/{status_id}',                  
+        'GET_AGENDAMENTO_DEFINIR_MEDICO': '/fiap-hmv/v1/agendamentos/{id_agendamento}/medicos/{id_medico}',     
+        'GET_AGENDAMENTO_ESTABELECIMENTOS': '/v1/agendamentos/pacientes/{id}/estabelecimentos/enderecos', 
+        'POST_PACIENTE_BUSCAR': '/v1/pacientes/busca'
     }
 
 };
